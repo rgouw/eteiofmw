8f6de77728fabe497f3a5082737cc5e1f8daeebf
makker-nl <martien.van.den.akker@darwin-it.nl>
2016-09-29 22:14:35 +0200

    installDB aangepast, gebruikmakend van db12c_env.sh. En toegevoegd dat de zips ook worden uitgepakt als nodig.

---
+++ scripts/db12c_env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo set Database 12c environment
+export ORACLE_BASE=/app/oracle
+export ORACLE_HOME=$ORACLE_BASE/product/database/dbhome_1
+export DATABASE_HOME=/data/oracle/oradata
--- scripts/installDB.sh
+++ scripts/installDB.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
-. $PWD/fmw12c_env.sh
-export DATABASE_HOME=/app/oracle/product/database/dbhome_1
-export DATABASE_INSTALL_HOME=/stage/database
-export DATABASE_RSP=/home/scripts/db.rsp
+. $PWD/db12c_env.sh
+export DB_ZIP_HOME=$PWD/../Zipped
+export DB_INSTALL_ZIP1=V46095-01_1of2.zip
+export DB_INSTALL_ZIP2=V46095-01_2of2.zip
+export DB_INSTALL_HOME=$PWD/../Extracted
+export DB_RSP=$PWD/db12c.rsp
 #
-echo DATABASE_HOME=$DATABASE_HOME
-if [ ! -f "$DATABASE_HOME/bin/oraping" ]; then
+echo ORACLE_HOME=$ORACLE_HOME
+if [ ! -f "$ORACLE_HOME/bin/oraping" ]; then
+  #Unzip DB12
+  if [ ! -f "$DB_INSTALL_HOME/database/runInstaller" ]; then
+    if [[ -f "$DB_ZIP_HOME/$DB_INSTALL_ZIP1" && -f "$DB_ZIP_HOME/$DB_INSTALL_ZIP2" ]]; then
+      echo Unzip $DB_ZIP_HOME/$DB_INSTALL_ZIP1 to $DB_INSTALL_HOME
+      unzip $DB_ZIP_HOME/$DB_INSTALL_ZIP1 -d $DB_INSTALL_HOME
+      echo Unzip $DB_ZIP_HOME/$DB_INSTALL_ZIP2 to $DB_INSTALL_HOME
+      unzip $DB_ZIP_HOME/$DB_INSTALL_ZIP2 -d $DB_INSTALL_HOME
+    else
+      echo $DB_ZIP_HOME/$DB_INSTALL_ZIP1 and/or $DB_ZIP_HOME/$DB_INSTALL_ZIP1 do not exist
+    fi  
+  else
+    echo $DB_INSTALL_HOME/database/runInstaller unzipped.
+  fi
   # Install Database
   echo Install Database
-  $DATABASE_INSTALL_HOME/runInstaller -silent -responseFile $DATABASE_RSP
+  $DB_INSTALL_HOME/database/runInstaller -silent -responseFile $DB_RSP
   sudo /data/oracle/oraInventory/orainstRoot.sh
-  sudo $DATABASE_HOME/root.sh
-  $DATABASE_HOME/cfgtoollogs/configToolAllCommands RESPONSE_FILE=$DATABASE_RSP
+  sudo $ORACLE_HOME/root.sh
+  $ORACLE_HOME/cfgtoollogs/configToolAllCommands RESPONSE_FILE=$DB_RSP
 else
   echo Database already installed
 fi
-
-
