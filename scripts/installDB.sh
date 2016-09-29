684b8c66f41abac4cdb0328b8ed168719e76cc09
makker-nl <martien.van.den.akker@darwin-it.nl>
2016-09-29 13:59:02 +0200

    Paden vervangen door env-vars.

--- scripts/installDB.sh
+++ scripts/installDB.sh
@@ -2,15 +2,16 @@
 . $PWD/fmw12c_env.sh
 export DATABASE_HOME=/app/oracle/product/database/dbhome_1
 export DATABASE_INSTALL_HOME=/stage/database
+export DATABASE_RSP=/home/scripts/db.rsp
 #
 echo DATABASE_HOME=$DATABASE_HOME
 if [ ! -f "$DATABASE_HOME/bin/oraping" ]; then
   # Install Database
   echo Install Database
-  $DATABASE_INSTALL_HOME/runInstaller -silent -responseFile /home/scripts/db.rsp  
+  $DATABASE_INSTALL_HOME/runInstaller -silent -responseFile $DATABASE_RSP
   sudo /data/oracle/oraInventory/orainstRoot.sh
-  sudo /app/oracle/product/database/dbhome_1/root.sh
-  /app/oracle/product/database/dbhome_1/cfgtoollogs/configToolAllCommands RESPONSE_FILE=/home/scrips/db.rsp
+  sudo $DATABASE_HOME/root.sh
+  $DATABASE_HOME/cfgtoollogs/configToolAllCommands RESPONSE_FILE=$DATABASE_RSP
 else
   echo Database already installed
 fi
