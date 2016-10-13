37f9b069ac597cdabe34a2313dfe06544989fa41
makker-nl <martien.van.den.akker@darwin-it.nl>
2016-10-13 20:45:41 +0200

    Changed reference to the response file and repaced _JAR by _BIN.

--- scripts/installOHS.sh
+++ scripts/installOHS.sh
@@ -3,7 +3,7 @@
 #
 export OHS_ZIP_HOME=$PWD/../Zipped
 export OHS_INSTALL_HOME=$PWD/../Extracted/stage
-export OHS_INSTALL_JAR=fmw_12.2.1.1.0_ohs_linux64.bin
+export OHS_INSTALL_BIN=fmw_12.2.1.1.0_ohs_linux64.bin
 export OHS_INSTALL_RSP=fmw_12.2.1.1.0_ohs.rsp
 export OHS_INSTALL_RSP_TPL=$OHS_INSTALL_RSP.tpl
 #export OHS_INSTALL_ZIP=fmw_12.2.1.1.0_wcportal_Disk1_1of1.zip
@@ -12,23 +12,23 @@ export OHS_INSTALL_RSP_TPL=$OHS_INSTALL_RSP.tpl
 if [[ -d "$FMW_HOME" && ! -d "$OHS_PROD_DIR/bin" ]]; then
   #
   #Unzip hs
-  if [ ! -f "$OHS_INSTALL_HOME/$OHS_INSTALL_JAR" ]; then
+  if [ ! -f "$OHS_INSTALL_HOME/$OHS_INSTALL_BIN" ]; then
     if [ -f "$OHS_ZIP_HOME/$OHS_INSTALL_ZIP" ]; then
-      echo Unzip $OHS_ZIP_HOME/$OHS_INSTALL_ZIP to $OHS_INSTALL_HOME/$OHS_INSTALL_JAR
+      echo Unzip $OHS_ZIP_HOME/$OHS_INSTALL_ZIP to $OHS_INSTALL_HOME/$OHS_INSTALL_BIN
       unzip -o $OHS_ZIP_HOME/$OHS_INSTALL_ZIP -d $OHS_INSTALL_HOME
     else
       echo $OHS_INSTALL_HOME/$OHS_INSTALL_ZIP does not exist!
     fi  
   else
-    echo $OHS_INSTALL_JAR already unzipped
+    echo $OHS_INSTALL_BIN already unzipped
   fi
-  if [ -f "$OHS_INSTALL_HOME/$OHS_INSTALL_JAR" ]; then
+  if [ -f "$OHS_INSTALL_HOME/$OHS_INSTALL_BIN" ]; then
     echo Substitute $OHS_INSTALL_RSP_TPL to $OHS_INSTALL_RSP
     envsubst < $OHS_INSTALL_RSP_TPL > $OHS_INSTALL_RSP
     echo Install WebCenter Portal 12cR2
-    $OHS_INSTALL_HOME/$OHS_INSTALL_JAR -silent -responseFile $PWD/$OHS_INSTALL_RSP
+    $OHS_INSTALL_HOME/$OHS_INSTALL_BIN -silent -responseFile $PWD/$OHS_INSTALL_RSP
   else
-    echo $OHS_INSTALL_JAR not available!./
+    echo $OHS_INSTALL_BIN not available!./
   fi
 else
   if [ ! -d "$FMW_HOME" ]; then
