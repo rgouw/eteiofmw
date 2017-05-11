2bfc9258d531986bfdac96b92a7f0bd615c2a98b
makker-nl <martien.van.den.akker@darwin-it.nl>
2017-05-11 12:20:43 +0200

    Bijwerken database host.

--- scripts/install/db12c_env.sh
+++ scripts/install/db12c_env.sh
@@ -5,7 +5,7 @@ export ORACLE_HOME=$ORACLE_BASE/product/database/dbhome_1
 export DATABASE_HOME=/data/oracle/oradata
 #
 export ORACLE_SID=orcl
-export DB_CONNECT_STR=dbhost:1521:${ORACLE_SID}
+export DB_CONNECT_STR=darlin-vce:1521:${ORACLE_SID}
 export PATH=/usr/sbin:$ORACLE_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$ORACLE_HOME/lib:/lib:/usr/lib;
 export CLASSPATH=$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib;
